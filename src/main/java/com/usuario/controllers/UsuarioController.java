d131d7c165a37501208c0eadbcb5c3f29e933ed6
luanmvcosta0 <luanmvcosta@mgail.com>
2025-03-10 12:17:12 -0300

    Criando a pasta Controller

---
+++ src/main/java/com/usuario/controllers/UsuarioController.java
@@ -0,0 +1,9 @@
+package com.usuario.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/usuario")
+public class UsuarioController {
+}
