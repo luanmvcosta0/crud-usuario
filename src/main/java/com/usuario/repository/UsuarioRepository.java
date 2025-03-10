1debe59e900da23658ad2582df2e2d9c4e6875ed
luanmvcosta0 <luanmvcosta@mgail.com>
2025-03-10 12:12:35 -0300

    Criando o repository

---
+++ src/main/java/com/usuario/repository/UsuarioRepository.java
@@ -0,0 +1,9 @@
+package com.usuario.repository;
+
+import com.usuario.entities.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+}
